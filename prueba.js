d87deb8368a2ec6dda81b51db18962cd7683de89
manu3 <44761989+manu3@users.noreply.github.com>
2018-12-05 19:53:46 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,30 @@
+ // JavaScript source code
+ function inicializar() {
+    //Opciones del mapa
+    var OpcionesMapa = {
+        center: new google.maps.LatLng(38.3489719, -0.4780289000000266),
+        mapTypeId: google.maps.MapTypeId.SATELLITE, //ROADMAP  SATELLITE HYBRID TERRAIN
+        mapMaker: true,
+        zoom: 16
+    };
+ 
+    var miMapa;
+    //constructor
+    miMapa = new google.maps.Map(document.getElementById('mapa'), OpcionesMapa);
+
+    //Añadimos el marcador
+    var Marcador = new google.maps.Marker({
+                    position: new google.maps.LatLng(38.3489719, -0.4780289000000266),
+                    map: miMapa,
+                    title:"Santa Barbara"
+                });
+}
+ 
+function CargaScript() {
+    var script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDjUlVzErtcnc8NA2PY4SyxDOcZf3az6DY&callback=inicializar';
+    document.body.appendChild(script);                 
+}
+
+///window.onload =CargaScript();
+//window.onload = CargaScript;
